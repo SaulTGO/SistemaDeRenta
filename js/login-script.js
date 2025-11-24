8aced15b4edb06c55ea45b126dbbbc23cb9d64f4
Saul Gonzalez <gonzalezolisaul23@gmail.com>
2025-11-23 21:55:09 -0600

    Release 2.0.2
    Correciones en linea 11 login-script.js

--- js/login-script.js
+++ js/login-script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     const loginForm = document.getElementById('loginForm');
-    const usernameInput = document.getElementById('correo');
+    const usernameInput = document.getElementById('username');
     const passwordInput = document.getElementById('password');
 
     // Manejar el envío del formulario
